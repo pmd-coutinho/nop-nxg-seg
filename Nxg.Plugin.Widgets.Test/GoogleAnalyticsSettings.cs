cf6e8630b99941405ee56d1098ee8a311b39666b
Pedro Coutinho <pmd.coutinho@gmail.com>
2024-01-05 13:37:54 +0000

    Custom plugin

---
+++ Nxg.Plugin.Widgets.Test/GoogleAnalyticsSettings.cs
@@ -0,0 +1,19 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nxg.Plugin.Widgets.Test
+{
+    public class GoogleAnalyticsSettings : ISettings
+    {
+        public string GoogleId { get; set; }
+        public string TrackingScript { get; set; }
+        public bool EnableEcommerce { get; set; }
+        public bool UseJsToSendEcommerceInfo { get; set; }
+        public bool IncludingTax { get; set; }
+        public string WidgetZone { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to include customer identifier to script
+        /// </summary>
+        public bool IncludeCustomerId { get; set; }
+    }
+}
---
+++ Nxg.Plugin.Widgets.Test/Models/ConfigurationModel.cs
@@ -0,0 +1,34 @@
+﻿using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nxg.Plugin.Widgets.Test.Models
+{
+    public record ConfigurationModel : BaseNopModel
+    {
+        public int ActiveStoreScopeConfiguration { get; set; }
+        
+        [NopResourceDisplayName("Plugins.Widgets.Test.GoogleId")]
+        public string GoogleId { get; set; }
+        public bool GoogleId_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Widgets.Test.EnableEcommerce")]
+        public bool EnableEcommerce { get; set; }
+        public bool EnableEcommerce_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Widgets.Test.UseJsToSendEcommerceInfo")]
+        public bool UseJsToSendEcommerceInfo { get; set; }
+        public bool UseJsToSendEcommerceInfo_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Widgets.Test.TrackingScript")]
+        public string TrackingScript { get; set; }
+        public bool TrackingScript_OverrideForStore { get; set; }
+        
+        [NopResourceDisplayName("Plugins.Widgets.Test.IncludingTax")]
+        public bool IncludingTax { get; set; }
+        public bool IncludingTax_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Widgets.Test.IncludeCustomerId")]
+        public bool IncludeCustomerId { get; set; }
+        public bool IncludeCustomerId_OverrideForStore { get; set; }
+    }
+}
